18480a8cee36f06d3fac1f61cb7f442ee0a2f968
Yanko Radev <85577992+yradev@users.noreply.github.com>
2023-11-18 16:50:58 +0200

    Add basic react files && sass integration

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+// gulpfile.js
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const globImporter = require('sass-glob-importer');
+
+gulp.task('style', function () {
+  return gulp.src('./src/scss/main.scss')
+    .pipe(sass({ importer: globImporter() }))
+    .pipe(gulp.dest('./public/assets/css'));
+});
+
+
+// Watch for changes and recompile Sass
+gulp.task('watch', function() {
+  gulp.watch('./src/scss/main.scss', gulp.series('style'));
+  gulp.watch('./src/scss/components/*', gulp.series('style'));
+});
+
+// Default task
+gulp.task('default', gulp.series('style', 'watch'));
